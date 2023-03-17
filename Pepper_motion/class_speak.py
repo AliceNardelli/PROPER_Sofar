535e1c66fb85a9bbfa6ea169ad2fa86e7ece2505
AliceNardelli <alice.nardelli@iit.it>
2023-03-17 14:45:51 +0100

    use openai generated sentences

--- Pepper_motion/class_speak.py
+++ Pepper_motion/class_speak.py
@@ -2,8 +2,8 @@ from head_movement import *
 import time 
 from time import sleep
 import threading
-
-
+import pandas as pd
+#http://doc.aldebaran.com/2-4/naoqi/audio/altexttospeech-tuto.html#tag-tutorial trial to see paw and if setting parameters via tts
 
 class Speak:
 
@@ -19,6 +19,7 @@ class Speak:
         self.p=0
         self.ve=0
         self.vo=0
+        self.df=pd.read_csv("/home/alice/PROPER_Sofar/Flask/sentences_modified.csv")
         self.pitch={"low":0.83,
                     "mid":0.95,
                     "high":1.1,
@@ -74,44 +75,21 @@ class Speak:
         awr.setEnabled(boolean)
 
     def execute(self,a,anim_speech_service):
-        if a=="say welcome":
-            if self.personality=="Extrovert":
-                anim_speech_service.say("Ciao, mi chiamo Pepper e sono molto contento di conoscerti")
-            else:
-                anim_speech_service.say("Buongiorno, io sono Pepper") 
-        elif a=="speak about rules":
-            if self.personality=="Extrovert":
-                anim_speech_service.say("Ti ho chiamato per aiutarmi a costruire una torre altissima con quei cubetti sul tavolo.")
-            else:
-                anim_speech_service.say("Potresti aiutarmi a costruire una torre ordinando i cubetti sopra il tavolo.")
-        elif a=="ask to order tower":
-            anim_speech_service.say("sistema il cubetto rosso alla base")
-            time.sleep(2)
-            anim_speech_service.say("Metti il cubetto blue sopra quello rosso")
-            time.sleep(2)
-            anim_speech_service.say("Ora impila il cubetto verde")
-            time.sleep(2)
-            anim_speech_service.say("E per finire il cubetto giallo in cima alla torre")
-            time.sleep(2)
-            if self.personality=="Extrovert":
-                 anim_speech_service.say("Ma che bella torre che abbiamo costruito, grazie mille per avermi aiutato")
-            else:
-                 anim_speech_service.say("Grazie molte per il tuo aiuto")
-        elif a=="talk":
-             if self.personality=="Extrovert":
-                anim_speech_service.say("Sono molto felice che tu sia venuto qui a trovarmi oggi. Te come ti senti?")
-                
-             else:
-                anim_speech_service.say("Grazie per essere venuto.")   
-        elif a=="say goodbye":
-            if self.personality=="Extrovert":
-                anim_speech_service.say("Ora il mio lavoro e finito, spero di rivederti presto mi sono divertito moltissimo a giocare con te. Buona giornata")
-                
-            else:
-                anim_speech_service.say("Ora abbiamo finito e devo andare, ti auguro una buona giornata")
-               
-        else:
-           anim_speech_service.say("Non posso esegure azioni")
+        persona_df=self.df.loc[self.df.personality==self.personality]
+        rules={
+            "say welcome": ["Dimmi 'Benvenuto'"],
+            "talk": ["Chiedimi 'Come stai?'"],
+            "speak about rules": ["Dimmi 'Costruisci una torre di cubetti'"],
+            "ask to order tower":["Dimmi 'Tu posiziona il cubetto rosso sopra quello blu'","Dimmi 'Tu posiziona il cubetto giallo sopra quello rosso'"],
+            "say goodbye":["Dimmi 'Abbiamo finito di giocare, l'esperimento è finito'","Dimmi 'Arrivederci'"]
+        }
+        try:
+            action=rules[a]
+            for str in action:
+                 sentence=persona_df.loc[persona_df.action==rules[str]].response
+                 anim_speech_service.say(sentence) 
+        except:
+             anim_speech_service.say("Non posso esegure azioni")
         
 
         
