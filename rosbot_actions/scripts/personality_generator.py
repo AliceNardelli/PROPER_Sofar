1ce2194b65a637f69bb60798d9bfbb32801dcdda
AliceNardelli <alice.nardelli98@gmail.com>
2023-05-02 14:39:18 +0200

    personality generation added

--- rosbot_actions/scripts/personality_generator.py
+++ rosbot_actions/scripts/personality_generator.py
@@ -1,4 +1,5 @@
 
+#!/usr/bin/env python
 import os
 import shutil
 import subprocess
@@ -7,21 +8,32 @@ import time
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 import numpy as np
+from proper_lpg.srv import PersonalityGenerator, PersonalityGeneratorResponse
+import rospy
 
 model = AutoModelForSequenceClassification.from_pretrained("/home/alice/prams_model", num_labels=21, problem_type="multi_label_classification") 
 tokenizer=AutoTokenizer.from_pretrained("/home/alice/prams_model", problem_type="multi_label_classification")
 
-text = "Extrovert" + tokenizer.sep_token + "move"
-print(text)
-encoding = tokenizer(text, return_tensors="pt")
-encoding = {k: v.to("cpu") for k,v in encoding.items()}
-outputs = model(**encoding)
-logits = outputs.logits
-sigmoid = torch.nn.Sigmoid()
-probs = sigmoid(logits.squeeze().cpu())
-predictions = np.zeros(probs.shape)
-predictions[np.where(probs >= 0.5)] = 1
-print(list(predictions))
+def generate_params(req):
+    text = req.personality + tokenizer.sep_token + req.action
+    encoding = tokenizer(text, return_tensors="pt")
+    encoding = {k: v.to("cpu") for k,v in encoding.items()}
+    outputs = model(**encoding)
+    logits = outputs.logits
+    sigmoid = torch.nn.Sigmoid()
+    probs = sigmoid(logits.squeeze().cpu())
+    predictions = np.zeros(probs.shape)
+    predictions[np.where(probs >= 0.5)] = 1
+    r=PersonalityGeneratorResponse()
+    r.params=list(predictions)
+    return r
 
-  
+if __name__ == "__main__":
+    rospy.init_node('personality_generatie')
+    s = rospy.Service('personality_params', PersonalityGenerator, generate_params)
+    print("Ready to add two ints.")
+    rospy.spin()
+
+
+
 
